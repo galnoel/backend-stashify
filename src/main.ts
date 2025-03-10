22cc8df88a5e12a7f0b4c7df31d39a9d63eff676
Your Name <galnoelpeterrindengan@gmail.com>
2025-03-10 12:15:29 +0800

    fix: port for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { hostname } from 'os';
 dotenv.config();
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
