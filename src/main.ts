49a08c483556b90df289edb4965e5748973110cb
Your Name <galnoelpeterrindengan@gmail.com>
2025-03-15 10:32:22 +0800

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { hostname } from 'os';
+import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Local development
+      'https://stashify-pi.vercel.app/' // Production frontend
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
